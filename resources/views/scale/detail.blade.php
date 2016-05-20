bb625d89777e58e8a82c669441852e8c5037e249
David Hart <davidhart@pond5.com>
2016-05-20 06:13:03 +0100

    Add mobile viewport tags

--- resources/views/scale/detail.blade.php
+++ resources/views/scale/detail.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet" type="text/css">
